2c37a7127f5407cfc1cd1b932ef874dfa6713e0f
Retoriax <juliansammet@gmail.com>
2023-03-28 11:48:26 +0200

    finished prototype

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Element} from "@angular/compiler";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  ngOnInit(): void {
+    const list = document.querySelectorAll('.list');
+    function activeLink(this: HTMLElement){
+      list.forEach((item) =>
+        item.classList.remove("active"));
+        this.classList.add("active");
+    }
+
+    list.forEach((item) =>
+      item.addEventListener('click', activeLink));
+  }
 
 }
