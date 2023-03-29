1d41bfab401d1b90c4caf6e5164df03b96b79c2e
Julian <juliansammet@gmail.com>
2023-03-29 02:12:22 +0200

    small changes to navbar css and added minZoom to Map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -29,6 +29,7 @@ export class MapComponent implements OnInit {
         this.map = L.map('map').setView([49.4953024,  8.5220529], 15);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
+          minZoom:10,
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
       });
@@ -37,6 +38,7 @@ export class MapComponent implements OnInit {
     this.map = L.map('map').setView([longitude, latitude], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
+      minZoom:10,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
     L.marker([longitude, latitude]).addTo(this.map)
