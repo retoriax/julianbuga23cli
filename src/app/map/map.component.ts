c3e4d48022abba23dbbf4f14b8abf6d625753d76
Denis <denismaric2000@gmail.com>
2023-04-03 16:06:32 +0200

    Alle Marker werden angezeigt

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,18 +15,22 @@ export class MapComponent implements OnInit {
   constructor(private bugapointservice: BugapointServiceService) {
   }
   ngOnInit() {
+    this.map = L.map('map').setView([49.4793, 8.49589], 15);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      minZoom:10,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
 
-    this.bugapointservice.findAll().subscribe((data: Bugapoint[]) => {
-      this.bugapoints = data;
-    });
     this.bugapointservice.getData().subscribe((data: any) => {
-        const latitude = data[0].latitude;
-        const longitude = data[0].longitude;
-        this.showMarker(latitude, longitude);
+        this.bugapoints = data;
+        for (const bugapoint of this.bugapoints) {
+          this.showMarker(bugapoint.latitude, bugapoint.longitude, bugapoint.title);
+        }
       },
       (error: any) => {
         console.error('An error occurred:', error);
-        this.map = L.map('map').setView([49.4953024,  8.5220529], 15);
+        this.map = L.map('map').setView([49.4793, 8.49589], 15);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
           minZoom:10,
@@ -34,14 +38,8 @@ export class MapComponent implements OnInit {
         }).addTo(this.map);
       });
   }
-  showMarker(latitude: number, longitude: number) {
-    this.map = L.map('map').setView([longitude, latitude], 15);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      minZoom:10,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
+  showMarker(latitude: number, longitude: number, title: string) {
     L.marker([longitude, latitude]).addTo(this.map)
-      .bindPopup('<b>Haupteingang Luisenpark</b><br />').openPopup().addTo(this.map);
+      .bindPopup(title).openPopup().addTo(this.map);
   }
 }
