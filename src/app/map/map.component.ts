b957b46e7a6a6e22d648a2dc6f5e0ba0c5a5eea9
Maximilian König <mkoenig2@stud.hs-heilbronn.de>
2023-04-12 01:09:37 +0200

    basic code for mapping

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 import {Bugapoint} from "../model/bugapoint";
 import {BugapointServiceService} from "../service/bugapoint-service.service";
 
@@ -11,6 +12,11 @@ import {BugapointServiceService} from "../service/bugapoint-service.service";
 export class MapComponent implements OnInit {
   map:any
   bugapoints: Bugapoint[];
+
+  p1:Bugapoint;
+  p2:Bugapoint;
+  p3:Bugapoint;
+  points:Bugapoint[];
 
   constructor(private bugapointservice: BugapointServiceService) {
   }
@@ -45,6 +51,26 @@ export class MapComponent implements OnInit {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
       });
+
+
+
+
+    this.p1 = new Bugapoint();
+    this.p1.latitude = 49.48345304381661;
+    this.p1.longitude = 8.49525170232847;
+
+    this.p2= new Bugapoint();
+    this.p2.latitude = 49.48557417009709;
+    this.p2.longitude = 8.490393677754632;
+
+    this.p3 = new Bugapoint();
+    this.p3.latitude = 49.48185249642774;
+    this.p3.longitude = 8.4991549728166;
+
+    this.points = [this.p2, this.p1];
+
+    this.showRoute(this.points);
+
   }
 
 
@@ -58,5 +84,26 @@ export class MapComponent implements OnInit {
   showMarker(latitude: number, longitude: number, title: string) {
     L.marker([longitude, latitude]).addTo(this.map)
       .bindPopup(title).openPopup().addTo(this.map);
+  }
+
+  /**
+   * Draws a route on the map between the points.
+   *
+   * @param points Points
+   */
+  showRoute(points:Bugapoint[]) {
+    const waypoints = points.map(point => {
+      return {
+        latLng: L.latLng(point.latitude, point.longitude)
+      };
+    });
+
+    L.Routing.control({
+      waypoints: waypoints,
+      router: L.Routing.osrmv1({
+        serviceUrl: 'http://localhost:5000/route/v1',
+        profile: 'foot'
+      })
+    }).addTo(this.map);
   }
 }
