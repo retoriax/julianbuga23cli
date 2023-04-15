00484dabe110d033392e6f4f4c49fa835cfe5c64
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-15 20:26:28 +0200

    "use location" button

--- src/app/adminpanel-bugapointlist/adminpanel-bugapointlist.component.ts
+++ src/app/adminpanel-bugapointlist/adminpanel-bugapointlist.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Bugapoint} from "../model/bugapoint";
 import {BugapointService} from "../service/bugapoint.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-adminpanel-bugapointlist',
@@ -9,15 +10,39 @@ import {BugapointService} from "../service/bugapoint.service";
 })
 export class AdminpanelBugapointlistComponent implements OnInit {
 
+  formGroup: FormGroup;
   points: Bugapoint[];
 
-  constructor(private bugapointservice: BugapointService) {
+  lats: FormControl[];
+  longs: FormControl[];
+
+  constructor(private bugapointservice: BugapointService, private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      inputcontrol: new FormControl()
+    })
   }
 
   ngOnInit(): void {
     this.bugapointservice.findAll().subscribe((data: Bugapoint[]) => {
       this.points = data;
+      this.lats = new Array(this.points.length);
+      this.longs = new Array(this.points.length);
+      let i = 0;
+      for (let point of this.points) {
+        this.lats[i] = new FormControl('');
+        this.longs[i] = new FormControl('');
+        i++;
+      }
     });
+  }
+
+  getGeoLocation(i: number) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lats[i].setValue(position.coords.latitude + '');
+        this.longs[i].setValue(position.coords.longitude + '');
+      })
+    }
   }
 
 }
