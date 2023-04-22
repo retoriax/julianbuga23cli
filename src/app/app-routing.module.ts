4e6a56d76430f0c4df910c87f86cb41fce195a32
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-22 13:12:09 +0200

    added admin panels in again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MapComponent} from "./map/map.component";
-import {BlankComponent} from "./blank/blank.component";
+import {AdminpanelMenuComponent} from "./adminpanel-menu/adminpanel-menu.component";
+import {AdminpanelBugapointsComponent} from "./adminpanel-bugapoints/adminpanel-bugapoints.component";
+import {AdminpanelNewpointpanelComponent} from "./adminpanel-newpointpanel/adminpanel-newpointpanel.component";
 
 
 const routes: Routes = [
   { path: 'map', component: MapComponent},
-  { path: 'blank', component: BlankComponent},
+  { path: 'admin/menu', component: AdminpanelMenuComponent},
+  { path: 'admin/bugapoints', component: AdminpanelBugapointsComponent},
+  { path: 'admin/bugapoints/new', component: AdminpanelNewpointpanelComponent }
 ];
 
 @NgModule({
