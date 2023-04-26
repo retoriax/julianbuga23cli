8c72e69c15607d79425d4acb14c991d3ec97c1ed
nuri <hendennurullah@gmail.com>
2023-04-26 23:32:15 +0200

    http status log

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -134,6 +134,7 @@ export class MapComponent implements OnInit {
     let http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
+    console.log(http.status);
     return http.status == 404;
   }
 
