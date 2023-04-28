40ad42451ab9ceb4d47fafbd7bd632c56ecc26e5
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-28 16:35:25 +0200

    filled admin-bugapoint.service.ts with the methods

--- src/app/services/admin-services/admin-bugapoint.service.ts
+++ src/app/services/admin-services/admin-bugapoint.service.ts
@@ -1,9 +1,80 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Bugapoint} from "../../model/bugapoint";
+import {DatabaseSaveResponse} from "../DatabaseSaveResponse";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment.development";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminBugapointService {
 
-  constructor() { }
+  private subPath = '/api/v1/admin/bugapoint';
+
+  constructor(private http: HttpClient) { }
+
+
+
+  /**
+   * Sends a post request to the server to add a new bugapoint to the database.
+   *
+   * @param bugapoint
+   */
+  saveBugapoint(bugapoint: Bugapoint) {
+    // @ts-ignore
+    return this.http.post<DatabaseSaveResponse>(environment.backEndUrl + `${this.subPath}/save`, bugapoint)
+      .subscribe((data: any) => {
+        console.log(data);
+      });
+  }
+
+  /**
+   * Returns all discriminators.
+   */
+  getDiscriminators(): Observable<string[]> {
+    return this.http.get<string[]>(environment.backEndUrl + `${this.subPath}/discriminators`);
+  }
+
+  /**
+   * Updates the bugapoint.
+   *
+   * @param bugapoint bugapoint which gets updated
+   * @param newLat new latitude
+   * @param newLong new longitude
+   * @param newAdminId new admin id
+   * @param newDescription new description
+   */
+  async updateBugapoint(bugapoint: Bugapoint, newLat?: number, newLong?: number, newAdminId?: number,
+                        newDescription?: string): Promise<DatabaseSaveResponse> {
+
+    const url = environment.backEndUrl + `${this.subPath}/update` + `?bugaPointId=${bugapoint.id}
+     &newLat=${newLat !== undefined ? newLat : bugapoint.latitude}
+     &newLong=${newLong !== undefined ? newLong : bugapoint.longitude}
+     &newDescription=${newDescription !== undefined ? newDescription : bugapoint.description}
+     &newAdminId=${newAdminId !== undefined ? newAdminId : bugapoint.adminID}`;
+
+    try {
+      let response = await this.http.put(url, null).toPromise();
+      return response as DatabaseSaveResponse;
+    } catch (e) {
+      return new class implements DatabaseSaveResponse {
+        message: string;
+        success: boolean = false;
+      }
+    }
+  }
+
+
+  /**
+   * Deletes a bugapoint with the id.
+   *
+   * @param id identifier
+   */
+  deleteBugapointById(id: number) {
+    return this.http.delete<string[]>(environment.backEndUrl + `${this.subPath}/delete?id=${id}`)
+      .subscribe((data: any) => {
+        console.log(data)
+      });
+  }
 }
