a264e50d8d60f0d5c33b3611f4d818eb2cf775bb
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-02 23:24:41 +0200

    fix: no works on the first load also

--- src/app/admin-components/adminpanel-bugapointlist/adminpanel-bugapointlist.component.ts
+++ src/app/admin-components/adminpanel-bugapointlist/adminpanel-bugapointlist.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Bugapoint} from "../../model/bugapoint";
-import {FormControl, FormGroup} from "@angular/forms";
 import {BugapointService} from "../../services/bugapoint.service";
 import {Admin} from "../../model/admin";
 import {AdminService} from "../../services/admin.service";
+import {lastValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-admin-components-bugapointlist',
@@ -12,42 +12,16 @@ import {AdminService} from "../../services/admin.service";
 })
 export class AdminpanelBugapointlistComponent implements OnInit {
 
-  formGroup: FormGroup;
   points: Bugapoint[];
   admins: Admin[];
-
-  lats: FormControl[];
-  longs: FormControl[];
 
   constructor(private bugapointservice: BugapointService, private adminservice: AdminService) {
 
   }
 
-  ngOnInit(): void {
-    this.adminservice.findAll().subscribe((data: Admin[]) => {
-      this.admins = data;
-      console.log(this.admins)
-    })
-
-    this.bugapointservice.findAll().subscribe((data: Bugapoint[]) => {
-      this.points = data;
-    });
-
-  }
-
-
-  /**
-   * Fills location input fields with present user location.
-   *
-   * @param i Index to find the right input fields
-   */
-  getGeoLocation(i: number) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.lats[i].setValue(position.coords.latitude + '');
-        this.longs[i].setValue(position.coords.longitude + '');
-      })
-    }
+  async ngOnInit() {
+    this.admins = await lastValueFrom(this.adminservice.findAll())
+    this.points = await lastValueFrom(this.bugapointservice.findAll())
   }
 
 }
