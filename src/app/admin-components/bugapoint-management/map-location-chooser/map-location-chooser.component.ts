46c27a1c6cc768bc284956f7a5d771281c81cb07
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-15 21:25:31 +0200

    generated component

---
+++ src/app/admin-components/bugapoint-management/map-location-chooser/map-location-chooser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-location-chooser',
+  templateUrl: './map-location-chooser.component.html',
+  styleUrls: ['./map-location-chooser.component.css']
+})
+export class MapLocationChooserComponent {
+
+}
