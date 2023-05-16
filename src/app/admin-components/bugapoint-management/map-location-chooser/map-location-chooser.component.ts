057cfd029fbf0d0821272e8a43b3d2b6acc14d35
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-16 19:08:34 +0200

    loads in bugapoint in mapchooser component

--- src/app/admin-components/bugapoint-management/map-location-chooser/map-location-chooser.component.ts
+++ src/app/admin-components/bugapoint-management/map-location-chooser/map-location-chooser.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import * as L from "leaflet";
+import {ActivatedRoute} from "@angular/router";
+import {BugapointService} from "../../../services/bugapoint.service";
+import {AdminBugapointService} from "../../../services/admin-services/admin-bugapoint.service";
+import {Bugapoint} from "../../../model/bugapoint";
+import {lastValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-map-location-chooser',
@@ -10,11 +15,26 @@ export class MapLocationChooserComponent implements OnInit{
 
   map: any;
 
-  constructor() {
-    console.log("HIER HIER")
+  point: Bugapoint[];
+
+  latitude: number;
+  longitude: number;
+
+  constructor(private route: ActivatedRoute, private bugapointService: BugapointService,
+              private adminbugapointService: AdminBugapointService) {
   }
 
   ngOnInit(): void {
+
+
+
+    this.route.queryParams.subscribe(async (params) => {
+      const points$ = this.bugapointService.findAll("whereId=" + params['bugaPointId'])
+      this.point = await lastValueFrom(points$)
+
+      console.log(this.point)
+    })
+
     this.map = L.map('map');
     this.map.setView([49.48374114628255, 8.494567173596847], 15);
 
