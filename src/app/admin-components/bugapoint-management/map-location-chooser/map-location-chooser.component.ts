0ac1db229ab9b13fe7263de8c8de1adae2b3ecd8
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-16 20:08:15 +0200

    map now clickable and a marker on new position appeares, also the input field update

--- src/app/admin-components/bugapoint-management/map-location-chooser/map-location-chooser.component.ts
+++ src/app/admin-components/bugapoint-management/map-location-chooser/map-location-chooser.component.ts
@@ -5,6 +5,7 @@ import {BugapointService} from "../../../services/bugapoint.service";
 import {AdminBugapointService} from "../../../services/admin-services/admin-bugapoint.service";
 import {Bugapoint} from "../../../model/bugapoint";
 import {lastValueFrom} from "rxjs";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-map-location-chooser',
@@ -14,35 +15,56 @@ import {lastValueFrom} from "rxjs";
 export class MapLocationChooserComponent implements OnInit{
 
   map: any;
+  newPositionMarker: L.Marker
+  newPosition: any;
 
-  point: Bugapoint[];
+  point: Bugapoint;
 
-  latitude: number;
-  longitude: number;
+  latForm = new FormControl
+  longForm = new FormControl
+  isNewLocationSet = true;
 
   constructor(private route: ActivatedRoute, private bugapointService: BugapointService,
               private adminbugapointService: AdminBugapointService) {
   }
 
+
   ngOnInit(): void {
-
-
-
     this.route.queryParams.subscribe(async (params) => {
-      const points$ = this.bugapointService.findAll("whereId=" + params['bugaPointId'])
-      this.point = await lastValueFrom(points$)
+      const points = await lastValueFrom(this.bugapointService.findAll("whereId=" + params['bugaPointId']))
+      this.point = points[0]
+
+      this.latForm.setValue(this.point.latitude + '')
+      this.longForm.setValue(this.point.longitude + '')
 
       console.log(this.point)
+
+      this.map = L.map('map');
+
+      console.log(this.point.latitude)
+      L.marker([this.point.latitude, this.point.longitude]).addTo(this.map)
+      this.map.setView([this.point.latitude, this.point.longitude], 16);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        minZoom:10,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(this.map);
+
+      this.map.on('click', (e: any) => {
+        this.isNewLocationSet = false;
+
+        var coord = this.newPosition = e.latlng;
+        this.latForm.setValue(coord.lat + '');
+        this.longForm.setValue(coord.lng + '');
+
+        if (this.newPositionMarker != null) {
+          this.map.removeLayer(this.newPositionMarker)
+        }
+        this.newPositionMarker = L.marker([coord.lat, coord.lng]).addTo(this.map);
+
+      })
     })
-
-    this.map = L.map('map');
-    this.map.setView([49.48374114628255, 8.494567173596847], 15);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      minZoom:10,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
   }
 
 }
