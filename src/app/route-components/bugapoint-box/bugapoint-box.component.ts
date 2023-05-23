f5418793f9ab5b637193d5d8abb173891ddf041f
Peter Stibitz <pstibitz@stud.hs-heilbronn.de>
2023-05-23 03:58:54 +0200

    set snackbar to top

--- src/app/route-components/bugapoint-box/bugapoint-box.component.ts
+++ src/app/route-components/bugapoint-box/bugapoint-box.component.ts
@@ -69,7 +69,7 @@ export class BugapointBoxComponent {
   unableToAddHighlightRouteBugapoint() {
     // set highlight to true
     this.unableToAddHighlight = true;
-    this.snackBar.open(this.point.title + 'ist schon vorhanden!', '', {
+    this.snackBar.open(this.point.title + ' ist schon vorhanden!', '', {
       duration:3000,
       verticalPosition:'top'
     });
@@ -86,7 +86,7 @@ export class BugapointBoxComponent {
   mergeHighlightRouteBugapoint() {
     // set highlight to true
     this.mergeHighlight = true;
-    this.snackBar.open(this.point.title + 'wurde mit dem selben Routenpunkt vereint!', '', {
+    this.snackBar.open(this.point.title + ' wurde mit dem selben Routenpunkt vereint!', '', {
       duration:3000,
       verticalPosition:'top'
     });
