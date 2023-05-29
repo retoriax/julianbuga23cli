dbcec1b32279789dc075486fdd3c570a471f04da
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-30 00:31:40 +0200

    env to local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  //backEndUrl: "http://localhost:8080/api/v1/"
-  backEndUrl: "https://bugamapspringboot-production-5b55.up.railway.app/api/v1"
+  backEndUrl: "http://localhost:8080/api/v1/"
+  //backEndUrl: "https://bugamapspringboot-production-5b55.up.railway.app/api/v1"
 };
