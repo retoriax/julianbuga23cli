8a5414d576504aedf4fa45a62ccb66fd1bc1be5b
Julian <juliansammet@gmail.com>
2023-05-30 17:57:14 +0200

    first implementation

---
+++ src/app/admin-components/report-show/report-show.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-show',
+  templateUrl: './report-show.component.html',
+  styleUrls: ['./report-show.component.css']
+})
+export class ReportShowComponent {
+
+}
